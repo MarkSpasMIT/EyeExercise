cb1556476007ee59710a35cfde6705d1f3bf4477
markspas <122735234+MarkSpasMIT@users.noreply.github.com>
2023-04-11 02:36:49 +0200

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,13 @@
+// From the MIT Program "Certificate in Coding: Full Stack Development with MERN, December 2022 cohort, week 8
+
+const balls = document.getElementsByClassName('ball');
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+};
